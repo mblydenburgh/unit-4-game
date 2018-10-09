79217a628926ef9ad90c500a7529f4b067fea59d
M Blydenburgh <mike.blydenburgh@gmail.com>
2018-10-09 07:19:53 -0400

    begin layout

---
+++ assets/javascript/game.js
@@ -0,0 +1,24 @@
+const characters = [
+    {name: "obi-wan",
+     hp:120,
+     attack:8,
+     counter:20
+    },
+    {name: "luke",
+     hp:100,
+     attack:8,
+     counter:20
+    },
+    {name: "d-maul",
+     hp:180,
+     attack:5,
+     counter:25
+    },
+    {name: "d-sidious",
+     hp:150,
+     attack:6,
+     counter:20
+    },
+];
+
+console.log(characters)
