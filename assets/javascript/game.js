3c3b3d6886376995ba49e8eb4a01263f289cb99c
Mike Blydenburgh <mike.blydenburgh@gmail.com>
2018-10-09 15:54:31 -0400

    made character cards

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,24 +1,61 @@
 const characters = [
-    {name: "obi-wan",
+    {name: "Obi-Wan Kenobi",
      hp:120,
      attack:8,
-     counter:20
+     counter:20,
+     url:"assets/images/obiwan.jpg"
     },
-    {name: "luke",
+    {name: "Luke Skywalker",
      hp:100,
      attack:8,
-     counter:20
+     counter:20,
+     url:"assets/images/luke.jpg"
     },
-    {name: "d-maul",
+    {name: "Darth Maul",
      hp:180,
      attack:5,
-     counter:25
+     counter:25,
+     url:"assets/images/maul.jpeg"
     },
-    {name: "d-sidious",
+    {name: "Darth Sidious",
      hp:150,
      attack:6,
-     counter:20
+     counter:20,
+     url:"assets/images/sidious.png"
     },
 ];
 
-console.log(characters)
+//Define jQuery selectors
+//
+
+let charactersDiv = $('#characters');
+
+// Define game variables
+//
+
+let playerCharacter;
+let enemyCharacter;
+
+// Define game functions
+//
+
+function createCard(char,i){
+    let title = $(`<div id="char${i}" class="character"><p>${char.name}</p><img src="${char.url}"><p>${char.hp}</p></div>`);
+    return charactersDiv.append(title);
+
+}
+
+// Create the character cards for player to choose
+//
+
+let characterCards = characters.map((char,i)=>{
+    return createCard(char,i);
+})
+
+// Create listen event to select character
+//
+
+$('#char0').click(()=>{
+    playerCharacter = this;
+    console.log(playerCharacter);
+})
