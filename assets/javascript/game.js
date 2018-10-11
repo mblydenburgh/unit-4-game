744fa3f9372405e076224949553148157f5f0d8a
M Blydenburgh <mike.blydenburgh@gmail.com>
2018-10-10 21:27:47 -0400

    moves selected character and defenders to correct positions

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -29,29 +29,44 @@ const characters = [
 //
 
 let charactersDiv = $('#characters');
+let defendersDiv = $('#defender');
 
 
 // Define game variables
 //
 
 let playerCharacter;
-let enemyCharacter;
+let enemyCharacters;
 
 // Define game functions
 //
 
+// dynamically creates characters to choose based on characters array of objects
 function createCard(char,i){
-    let title = $(`<div id="char${i}" class="character"><p>${char.name}</p><img src="${char.url}"><p>${char.hp}</p></div>`);
-    return charactersDiv.append(title);
+    let characterCard = $(`<div id="char${i}" class="character"><p>${char.name}</p><img src="${char.url}"><p>${char.hp}</p></div>`);
+    return charactersDiv.append(characterCard);
 
 }
 
-// Create the character cards for player to choose
+
+//note: ask why the [0] index is needed for player
+//once player selects a character, the chosen character remains in the characters div, while the remaining characters are moved to the defender area
+function setupCharacters(player,enemy){
+    console.log(player[0]);
+    console.log(enemy);
+    charactersDiv.empty();
+    charactersDiv.append(`<div id="char" class="character"><p>${player[0].name}</p><img src="${player[0].url}"><p>${player[0].hp}</p></div>`);
+    let enemyCards = enemy.map((enemy,i)=>{
+        return defendersDiv.append(`<div id="defender${i}" class="character"><p>${enemy.name}</p><img src="${enemy.url}"><p>${enemy.hp}</p></div>`)
+    })
+}
+
+// Create the character cards for player to choose by looping over the characters array
 //
 
 let characterCards = characters.map((char,i)=>{
     return createCard(char,i);
-})
+});
 
 // Create listen events to select character
 //
@@ -60,24 +75,33 @@ let lukeCard = $('#char1');
 let maulCard = $('#char2');
 let sidiousCard = $('#char3');
 
-
 obiWanCard.click(()=>{
     console.log(`clicked ${characters[0].name}`);
-    playerCharacter = characters[0];
+    playerCharacter = characters.splice(0,1);
+    console.log(`player: ${playerCharacter}`)
+    enemyCharacters = characters;
+    console.log(`enemies: ${characters}`)
     //console.log(playerCharacter);
+    setupCharacters(playerCharacter,enemyCharacters);
 });
 
 lukeCard.click(()=>{
     console.log(`clicked ${characters[1].name}`);
-    playerCharacter = characters[1];
+    playerCharacter = characters.splice(1,1);
+    enemyCharacters = characters;
+    setupCharacters(playerCharacter,enemyCharacters);
 });
 
 maulCard.click(()=>{
     console.log(`clicked ${characters[2].name}`);
-    playerCharacter = characters[2];
+    playerCharacter = characters.splice(2,1);
+    enemyCharacters = characters;
+    setupCharacters(playerCharacter,enemyCharacters);
 });
 
 sidiousCard.click(()=>{
     console.log(`clicked ${characters[3].name}`);
-    playerCharacter = characters[3];
-})
+    playerCharacter = characters.splice(3,1);
+    enemyCharacters = characters;
+    setupCharacters(playerCharacter,enemyCharacters);
+});
